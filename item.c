6598238c4de42e8f22e06ea5aec4735a5ccadf1b
teapot123 <hjx14@mails.tsinghua.edu.cn>
2015-10-30 23:56:08 +0800

    Update item.c

--- item.c
+++ item.c
@@ -1,117 +1,121 @@
-#include "stdio.h"
 #include "math.h"
+#include "th_base.h"
 #define limitAngle 45
 #define TurningTime 500000
 #define ItemEatingTime 500000
 #define pi 3.1415926
-#define SensorNum 6
+#define SensorNum 4
+#define nearX 20
+#define nearY 20
 
-
-struct Item{                  //x,y是道具位置坐标
+struct Item{
     int x;
     int y;
 }items[4];
 
 int xl,yl,xc,yc;
-bool firstTime=true;
+int carDirectionX,carDirectionY;
+unsigned char firstTime=true;
 
 void initItems(){
-    int itemNum=0;                                                              //场上有效道具数
-    if(((input_data.raw_data[1]>>3)&7)==1) itemNum=1;
+    int i;
+    int itemNum;
+    if(((input_data.raw_data[1]>>3)&7)==0) itemNum=0;
+    else if(((input_data.raw_data[1]>>3)&7)==1) itemNum=1;
     else if(((input_data.raw_data[1]>>3)&7)==2) itemNum=2;
     else if(((input_data.raw_data[1]>>3)&7)==3) itemNum=3;
     else itemNum=4;
-    for(int i=0;i<itemNum;i++){
+    for(i=0;i<itemNum;i++){
         items[i].x=input_data.raw_data[11+2*i];
         items[i].y=input_data.raw_data[12+2*i];
     }
-    for(int i=itemNum;i<4;i++){
+    for(i=itemNum;i<4;i++){
         items[i].x=-1;
         items[i].y=-1;
     }
     
-    int ID=input_data.raw_data[0]>>6;                            // 选手ID
-    if(firstTime){                                              // (xl,yl): 上次记录的位置坐标; (xc,yc): 本次记录的位置坐标.
+    int ID=input_data.raw_data[0]>>6;
+    if(firstTime){
         xl=input_data.raw_data[4+2*ID];
         yl=input_data.raw_data[5+2*ID];
         xc=xl;
         yc=yl;
-        firstTime=false;
+        carDirectionX=0;
+        carDirectionY=0;
+        firstTime=0;
     }
     else{
         xl=xc;
         yl=yc;
         xc=input_data.raw_data[4+2*ID];
         yc=input_data.raw_data[5+2*ID];
+        if((xc!=xl)||(yc!=yl)) {
+            carDirectionX=xc-xl;
+            carDirectionY=yc-yl;
+        }
     }
 }
 
-double angle(int x,int y){                                            //返回该坐标与小车连线和小车现在行驶方向的夹角（角度值）
-    int carDirectionX= xc-xl, carDirectionY=yc-yl;
+double angle(int x,int y){
     int targetDirectionX= x-xc, targetDirectionY=y-yc;
-    if((carDirectionX==0&&carDirectionY==0)||(targetDirectionX==0&&targetDirectionY==0) return 0;      //避免下面计算出现错误
-    return acos((carDirectionX*carDirectionY+targetDirectionX*targetDirectionY)/((carDirectionX^2+carDirectionY^2)^0.5*(targetDirectionX^2+targetDirectionY^2)^0.5))/pi*180;
+    if((carDirectionX==0&&carDirectionY==0)||(targetDirectionX==0&&targetDirectionY==0)) return 0;
+    return acos((carDirectionX*carDirectionY+targetDirectionX*targetDirectionY)/(sqrt(carDirectionX*carDirectionX+carDirectionY*carDirectionY))*sqrt((targetDirectionX*targetDirectionX+targetDirectionY*targetDirectionY)))/pi*180;
 }
 
-double Distance(x,y){                                                  //返回该坐标与小车距离
-    return ((x-xc)^2+(y-yc)^2)^0.5;
+double Distance(int x,int y){
+    return sqrt((x-xc)*(x-xc)+(y-yc)*(y-yc));
 }
 
-int firstItem(){                                                     //返回在一定夹角范围内且离自己最近的道具
-    int firstItem=-1;
+int firstItem(){
+    int i;
+    int FirstItem;
     int minDistance=512;
-    for(int i=0;i<4;i++){
+    for(i=0;i<4;i++){
         if(items[i].x==-1||items[i].y==-1) break;
-        if(0<=(angle(items[i].x,items[i].y)&&(angle(items[i].x,items[i].y)<=limitAngle){
+        if((0<=angle(items[i].x,items[i].y))&&(angle(items[i].x,items[i].y)<=limitAngle)){
             if(Distance(items[i].x,items[i].y)<minDistance){
-                firstItem=i;
+                FirstItem=i;
                 minDistance=Distance(items[i].x,items[i].y);
             }
         }
     }
-    return firstItem;
+    return FirstItem;
 }
 
 void collectItems(int n){                                             
-  /* 捡道具的具体实施过程。以后可以加一个判断是要捡还是要躲避这个道具的条件。
-     比如自己已经有很好的道具但还没有机会使用，不希望新道具覆盖原来道具的时候，可以躲避新道具。
-  */
-
-
-    if(n==-1) return;                                                 //没有需要捡的道具，返回
+    int i;
+    if(n==-1) return;
     int j;
-    for(j=0;j<SensorNum;j++){                                         //如果撞到黑线，也返回
+    for(j=0;j<SensorNum;j++){
         if(GetLightSensor(i)==0) break;
     }
     if(j<SensorNum) return;
 
-    int carX= xc-xl, carY= yc-yl;                                          //小车方向
-    int targeX= items[n].x-xc, targetY=items[n].y-yc;                      //道具与小车连线的方向
+    int targetX= items[n].x-xc, targetY=items[n].y-yc;
 
-    if(abs(targetX-xc)<=nearX&&abs(targetY-yc)<=nearY){                    //如果小车已经在道具周围，停一段时间
+    if(abs(targetX-xc)<=nearX&&abs(targetY-yc)<=nearY){
         SetMotor(0,0);
         SetMotor(1,0);
         DELAY_US(ItemEatingTime);
         return;
     }
 
-    if((carX==0&&carY==0)||(targetX==0&&targetY==0) return;            //避免下面计算角度时出错
-
-    double angle1, angle2;                                            //以下一大段用来计算该向左转还是向右转T_T, 只会分类讨论T_T
+    if((carDirectionX==0&&carDirectionY==0)||(targetX==0&&targetY==0)) return;
+    double angle1, angle2;
     if(carY>=0){
-        angle1= acos(carX/((carX^2+carY^2)^0.5))/pi*180;             
+        angle1= acos(carDirectionX/(sqrt(carDirectionX*carDirectionX+carDirectionY*carDirectionY)))/pi*180;
     }
     else{
-        angle1= acos(carX/((carX^2+carY^2)^0.5))/pi*180+180;
+        angle1= acos(carDirectionX/(sqrt(carDirectionX*carDirectionX+carDirectionY*carDirectionY)))/pi*180+180;
     }
     if(targetY>=0){
-        angle2=acos(targetX/((targetX^2+targetY^2)^0.5))/pi*180;
+    	 angle2= acos(targetX/(sqrt(targetX*targetX+targetY*targetY)))/pi*180;
     }
     else{
-        angle2=acos(targetX/((targetX^2+targetY^2)^0.5))/pi*180+180;
+    	 angle2= acos(targetX/(sqrt(targetX*targetX+targetY*targetY)))/pi*180+180;
     }
-    bool left;
-    double angle=angle(items[n].x,items[n].y);
+    unsigned char left;
+    double Angle=angle(items[n].x,items[n].y);
     if(angle1<180){
         if(angle1<angle2 && angle2<180+angle1) left=true;
         else left=false;
@@ -121,21 +125,21 @@ void collectItems(int n){
         else left=true;
     }
 
-    if(left){                                                         //向左转
-        SetMotor(1,100);
-        SetMotor(0,int(100-10/9*angle);                             //瞎编了一个轮子转速和小车要转过的角度的关系，不知道应该怎么算
+    if(left){
+        SetMotor(1,1000);
+        SetMotor(0,(int)(1000-100/9*Angle));
         DELAY_US(TurningTime);
     }
-    else{                                                             //向右转
-        SetMotor(0,100);
-        SetMotor(1,int(100-10/9*angle);
+    else{
+        SetMotor(0,1000);
+        SetMotor(1,(int)(1000-100/9*Angle));
         DELAY_US(TurningTime);
     }
 }
 
-void Items(){                                                        //main函数调用它就好啦
+void Items(void){
     UpdateData();
     initItems();                        
     collectItems(firstItem());   
-	return;
+    return;
 }
